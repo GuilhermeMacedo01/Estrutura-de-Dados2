0412d2c2401fce6777d6e73bc252ae0d5ca592fb
GuilhermeMacedo01 <guilhermemorais586@gmail.com>
2023-10-27 16:42:21 -0300

    27_10

---
+++ tarefaN02/Teste.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Teste.h"
+
+
+int main() {
+    // Inicialize a tabela hash com NULL
+    struct Cliente* table[TABLE_SIZE];
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        table[i] = NULL;
+    }
+
+    int opcao, chave;
+    int escolha;
+    int codigoBusca;
+    int codigoLivre;
+
+
+    do {
+        printf("\n1. Inserir cliente\n2. Buscar cliente\n3. Marcar posicao livre\n4. Imprimir todos o Clientes\n5. Sair\nEscolha uma alternativa: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                struct Cliente* novoCliente = criarCliente();
+                insert(novoCliente->nome, novoCliente->codigo, table);
+                printf("Cliente inserido na tabela.\n");
+                break;
+            
+            case 2:
+                int codigoBusca;
+                printf("Código do cliente a ser buscado: ");
+                scanf("%d", &codigoBusca);
+                struct Cliente* clienteEncontrado = search(codigoBusca, table);
+                if (clienteEncontrado != NULL) {
+                    printf("Cliente encontrado: %s, %d\n", clienteEncontrado->nome, clienteEncontrado->codigo);
+                } else {
+                    printf("Cliente com código %d não encontrado.\n", codigoBusca);
+                }
+                break;
+            
+            case 3:
+                int codigoLivre;
+                printf("Código do cliente a ser marcado como livre: ");
+                scanf("%d", &codigoLivre);
+                marcarComoLivre(codigoLivre, table);
+                printf("Posição marcada como livre.\n");
+                break;
+
+            case 4:
+                printTable(table);
+                break;
+        }
+    } while (opcao != 5);
+
+    return 0;
+}
---
+++ tarefaN02/Teste.h
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TABLE_SIZE 7
+
+// Estrutura para armazenar um elemento na tabela
+struct Cliente {
+    char nome[100];
+    int codigo;
+    int status;
+};
+
+// Função para criar um novo elemento
+struct Cliente* createNode(char nome[], int codigo) {
+    struct Cliente* newNode = (struct Cliente*)malloc(sizeof(struct Cliente));
+    strcpy(newNode->nome, nome);
+    newNode->codigo = codigo;
+    return newNode;
+};
+
+// Função para buscar um cliente pelo código
+struct Cliente* search(int codigo, struct Cliente* table[]) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (table[i] != NULL && table[i]->codigo == codigo) {
+            return table[i];
+        }
+    }
+    return NULL; // Cliente não encontrado
+}
+
+// Função para calcular o índice na tabela hash
+int hash(int key, int attempt, int size) {
+    return (key + attempt) % size;  // Use uma fórmula para tentar posições alternativas
+}
+
+// Função para marcar a posição
+void marcarPosicao(int index, int status, struct Cliente* table[]) {
+    if (index >= 0 && index < TABLE_SIZE) {
+        if (status == 0 || status == 1) {
+            table[index]->status = status;
+        }
+    }
+}
+
+
+//Função para inserir o usuário na tabela
+void insert(char nome[], int codigo, struct Cliente* table[]) {
+    int index = hash(codigo, 0, TABLE_SIZE); // Índice inicial
+
+    int attempt = 0;
+    while (table[index] != NULL && table[index]->status == 1) {
+        // Se a posição estiver ocupada, tente a próxima posição
+        attempt++;
+        index = hash(codigo, attempt, TABLE_SIZE);
+
+        // Se todas as posições estiverem ocupadas, saia
+        if (attempt >= TABLE_SIZE) {
+            printf("Tabela cheia. Não é possível inserir o cliente (%s, %d).\n", nome, codigo);
+            return;
+        }
+    }
+
+    table[index] = createNode(nome, codigo);
+    table[index]->status = 1; // Marcar a posição como ocupada
+}
+//Função para marcar a posição como livre
+void marcarComoLivre(int codigo, struct Cliente* table[]) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (table[i] != NULL && table[i]->codigo == codigo) {
+            table[i]->status = 0; // Marcar a posição como livre
+        }
+    }
+}
+
+// Função para criar um cliente com entrada do usuário
+struct Cliente* criarCliente() {
+    char nome[100];
+    int codigo;
+
+    printf("Nome do cliente: ");
+    scanf("%s", nome);
+
+    printf("Código do cliente: ");
+    scanf("%d", &codigo);
+
+    return createNode(nome, codigo);
+}
+
+// Função para imprimir a tabela hash
+void printTable(struct Cliente* table[]) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        printf("Tabela[%d]: ", i);
+        if (table[i] != NULL) {
+            printf("(%s, %d)\n", table[i]->nome, table[i]->codigo);
+        } else {
+            printf("NULL\n");
+        }
+    }
+}
+
